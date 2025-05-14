1ef0b32d029a16a27d4cb8327220b4d4dd7d420a
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-05-14 18:35:54 +0200

    best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
     else:
         best_score = max(a_dictionary.values())
-        return best_score
+        for key in a_dictionary:
+            if a_dictionary[key] == best_score:
+                return key
