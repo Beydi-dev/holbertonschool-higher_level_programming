7b990f57ab6a79ffd65dccca160f530f3dcab802
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-05-18 20:40:50 +0200

    roman numeral

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-def roman_to_int(s:"str")->"int":
-    number = 0
-    roman = {'M':1000, 'D':500, 'C': 100, 'L':50, 'X':10, 'V':5, 'I':1}
-    for i in range(len(s)-1):
-        if roman[s[i]] < roman[s[i+1]]:
-            number -= roman[s[i]]
-        else:
-            number += roman[s[i]]
-    return number + roman[s[-1]]
+#!/usr/bin/python3
 
+def roman_to_int(roman_string: str) -> int:
+    if not isinstance(roman_string, str):
+        return 0
+    roman = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
+    number = 0
+    for i in range(len(roman_string) - 1):
+        if roman[roman_string[i]] < roman[roman_string[i + 1]]:
+            number -= roman[roman_string[i]]
+        else:
+            number += roman[roman_string[i]]
+    return number + roman[roman_string[-1]]
 
 if __name__ == "__main__":
     roman = "DCXXI"
-    print(roman_to_int(roman))
+    print(roman_to_int(roman))  # 621
