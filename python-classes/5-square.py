dc2d0959235c99e41c4031ad28bda11406d3d344
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-05-20 10:50:45 +0200

    square #

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -33,6 +33,6 @@ class Square:
     def my_print(self):
         for i in range(self.__size):
             if self.__size == 0:
-                pass
+                print()
             else:
                 print("#" * self.__size)
