fb8e7e3bbdb3ee7ae7a0fc063b856b91e1929746
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-05-23 01:29:05 +0200

    Retry

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -14,18 +14,18 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
-        @property
-        def width(self):
-            return self.__width
+    @property
+    def width(self):
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            self.__width = width
+    @width.setter
+    def width(self, value):
+        self.__width = value
 
-        @property
-        def height(self):
-            return self.__height
+    @property
+    def height(self):
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            self.__height = height
+    @height.setter
+    def height(self, value):
+        self.__height = value
