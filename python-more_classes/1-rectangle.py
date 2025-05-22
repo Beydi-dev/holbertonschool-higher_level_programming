903f18b573cf76541a00229707e0783b97fabc49
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-05-23 01:42:48 +0200

    Retry

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ This module defines a Rectangle class.
 class Rectangle:
     """This class defines a rectangle with width and height"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
