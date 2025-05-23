d23b408ec00b499e08fcfdbd6fea4dad510d8246
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-05-23 11:32:37 +0200

    number of instances

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -6,7 +6,10 @@ This module defines a Rectangle class.
 
 class Rectangle:
     """This class defines a rectangle with width and height"""
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
+        Rectangle.number_of_instances += 1
         self.width = width
         self.height = height
 
@@ -56,4 +59,5 @@ class Rectangle:
         return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
