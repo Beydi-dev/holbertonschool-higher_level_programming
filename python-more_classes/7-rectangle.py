3cc7377249622fd320dfddbe3190d9e2cf888f9e
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-05-23 22:22:31 +0200

    print symbol

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle:
 
     @property
     def width(self):
-        return self.width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -24,11 +24,11 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.width = value
+        self.__width = value
 
     @property
     def height(self):
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -36,7 +36,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.height = value
+        self.__height = value
 
     def area(self):
         return (self.width * self.height)
