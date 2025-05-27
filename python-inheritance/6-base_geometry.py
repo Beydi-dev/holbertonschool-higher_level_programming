e7d104edca335de5a590e020db0c6911a3034ed7
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-05-27 17:16:30 +0200

    First Try

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A class BaseGeometry"""
+
+
+class BaseGeometry:
+    """A class with a public instance method"""
+    def area(self):
+        raise Exception("area() is not implemented")
