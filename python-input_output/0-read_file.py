370945d0eaeca1ca1155e2d560e7a994e19aca74
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-06-02 13:29:43 +0200

    with open

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""a function that reads a text file (UTF8) and prints it to stdout"""
+
 
 def read_file(filename=""):
+    """a function that reads a text file (UTF8) and prints it to stdout"""
     if filename.endswith(".txt"):
         with open(filename) as file:
             content = file.read()
