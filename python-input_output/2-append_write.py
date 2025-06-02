8def1f38bb4a4cc313cb8ed1aba85b4164d52c4f
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-06-02 14:23:37 +0200

    a

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,6 +6,6 @@ and returns the number of characters added:"""
 def append_write(filename="", text=""):
     """ appending a string at the end of a text file (UTF8)
     and returns the number of characters added:"""
-    with open(filename, 'w') as file:
-        file.append(text)
+    with open(filename, 'a') as file:
+        file.write(text)
         return len(text)
