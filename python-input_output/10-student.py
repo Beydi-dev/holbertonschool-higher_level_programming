b639e8c9b60f4a92d1d293b3e4acfd921873b5ef
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-06-03 13:46:58 +0200

    10 student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Class that defines a student"""
+
+
+class Student:
+    """Class that defines student with method"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (
+            isinstance(attrs, list)
+            and all(isinstance(attr, str) for attr in attrs)
+        ):
+            return {
+                key: getattr(self, key)
+                for key in attrs
+                if hasattr(self, key)
+            }
+        else:
+            return self.__dict__
