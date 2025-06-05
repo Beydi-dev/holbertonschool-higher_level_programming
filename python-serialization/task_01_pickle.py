56fce854f0fbbccd5a4cb6266a54473e6a388d3e
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-06-05 19:59:48 +0200

    pickle method

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -29,5 +29,5 @@ class CustomObject:
             with open(filename, "rb") as f:
                 obj = pickle.load(f)
             return obj
-        except (FileNotFoundError, pickle.PickleError):
+        except Exception:
             return None
