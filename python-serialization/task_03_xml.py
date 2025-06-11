346e783f5dd473d31ef6a9ffd86d745268dd449b
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-06-11 14:00:24 +0200

    XML

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Serializing and Desarializing XML"""
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    """serializing Python dictionary into XML"""
+    root = ET.Element("data")
+    for key, value in dictionary.items():
+        child = ET.SubElement(root, key).text = value
+
+    ET.ElementTree(root).write(filename)
+
+
+def deserialize_to_xml(filename):
+    """deserializing XML to Python dictionary"""
+    tree = ET.parse(filename)       # Loading the XML
+    root = tree.getroot()           # Retrieving <data> root
+    result = {}                     # Initializing an empty dict
+
+    for child in root:              # Read through each child tag
+        result[child.tag] = child.text  # Adding in the dict
+
+    return result
