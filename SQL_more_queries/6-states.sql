31041e24062f1a7d03412a7ccf9f9a2fd4692631
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-06-24 21:22:11 +0200

    task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- Creates a table if not exists with unique id
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS unique_id (id INT UNIQUE NOT NULL, name VARCHAR(256), NOT NULL);
