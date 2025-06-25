a53912000acce60855f1d1bbf60bbfe429bf9f1e
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-06-25 15:27:29 +0200

    task 7

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,4 @@
--- Creates a table if not exists with unique id
--- Creates the database and the table states
+-- Creates the table hbtn_0d_usa with table states.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(256) NOT NULL);
