dd895e52ba11e98b1cee779f48c5e0d7a5e837c7
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-06-25 15:36:09 +0200

    task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,6 @@
 -- Creates a database and a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-	-- We using primary key here because it is unique and null at the same time
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
