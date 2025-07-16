08c65ba0365835155533e2c0b97a8bbd59cac643
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-07-16 17:10:22 +0200

    6 et 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const num = parseInt(arg);
 if (isNaN(num)) {
   console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < num; i++) {
-        console.log('C is fun');
-    }
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
 }
