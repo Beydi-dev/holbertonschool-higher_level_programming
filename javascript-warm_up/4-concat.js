cf1b32e97a040701ae8c918c6e86c8946d256d4a
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-07-16 15:57:19 +0200

    3-args

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// prints the first argument passed
+
+const args = process.argv.slice(2);
+
+if (args[0] === undefined) {
+  console.log('No argument');
+} else if (args[0] !== undefined) {
+  console.log(args[0]);
+}
