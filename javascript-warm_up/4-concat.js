57c3fa5ad98b8e07ea7ed01bab5fd19c12c23bbd
Beydi-dev <beydi.coulibalyy@gmail.com>
2025-07-16 16:24:59 +0200

    4-concat

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 
-// prints the first argument passed
+//  prints two arguments passed, in the following format: “ is
 
 const args = process.argv.slice(2);
 
-if (args[0] === undefined) {
-  console.log('No argument');
-} else {
-  console.log(args[0]);
-}
+console.log(`${args[0]} is ${args[1]}`);
